8611500aee56faec072e64ceabae931690d69f2d
MelloBirkan <mgbirkan@icloud.com>
2024-10-29 22:21:31 -0300

    Moficado componente Image

--- app/src/main/java/com/example/caffeeco/MainActivity.kt
+++ app/src/main/java/com/example/caffeeco/MainActivity.kt
@@ -14,14 +14,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,43 +35,56 @@ import androidx.compose.ui.unit.dp
 import com.example.caffeeco.ui.theme.CaffeeCoTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContent {
-      CaffeeCoTheme {
-        Surface {
-          ProductItem()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CaffeeCoTheme {
+                Surface {
+                    ProductItem()
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun ProductItem(modifier: Modifier = Modifier) {
-  Column(
-    modifier
-      .height(250.dp)
-      .width(250.dp)
-  ) {
-    Box(
-      modifier
-        .height(50.dp)
-        .width(50.dp)
-        .background(Color.Blue)
-    )
+    Column(
+        modifier
+            .height(250.dp)
+            .width(250.dp)
+    ) {
+        Box(
+            modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xff6200ee),
+                            Color(0xff03dac5)
+                        )
+                    )
+                )
+        )
 
-    Image(
-      painter = painterResource(R.drawable.ic_launcher_background),
-      contentDescription = null
-    )
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier
+                .offset(y = (-50).dp)
+                .size(100.dp)
+                .clip(CircleShape)
+                .align(CenterHorizontally)
 
-    Text("Texto1")
+        )
 
-    Text("Texto1")
-  }
+        Text("Nome do Produto")
+
+        Text("Texto1")
+    }
 }
 
 
