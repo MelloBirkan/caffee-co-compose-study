4b4f7056c77615296bc7f7d54e254ee3ed0689f7
MelloBirkan <mgbirkan@icloud.com>
2024-11-03 01:55:19 -0300

    Ajustando os textos de forma inicial

--- app/src/main/java/com/example/caffeeco/MainActivity.kt
+++ app/src/main/java/com/example/caffeeco/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -24,67 +25,87 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.caffeeco.ui.theme.CaffeeCoTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CaffeeCoTheme {
-                Surface {
-                    ProductItem()
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      CaffeeCoTheme {
+        Surface {
+          ProductItem()
         }
+      }
     }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun ProductItem(modifier: Modifier = Modifier) {
-    Column(
-        modifier
-            .height(250.dp)
-            .width(250.dp)
+  Column(
+    modifier
+      .height(250.dp)
+      .width(250.dp)
+  ) {
+    Box(
+      modifier
+        .height(100.dp)
+        .fillMaxWidth()
+        .background(
+          brush = Brush.horizontalGradient(
+            colors = listOf(
+              Color(0xff6200ee),
+              Color(0xff03dac5)
+            )
+          )
+        )
     ) {
-        Box(
-            modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Color(0xff6200ee),
-                            Color(0xff03dac5)
-                        )
-                    )
-                )
-        )
-
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
-            contentDescription = null,
-            modifier
-                .offset(y = (-50).dp)
-                .size(100.dp)
-                .clip(CircleShape)
-                .align(CenterHorizontally)
-
-        )
-
-        Text("Nome do Produto")
-
-        Text("Texto1")
+      Image(
+        painter = painterResource(R.drawable.ic_launcher_background),
+        contentDescription = null,
+        modifier
+          .offset(y = 50.dp)
+          .size(100.dp)
+          .clip(CircleShape)
+          .align(BottomCenter)
+      )
     }
+
+    Spacer(
+      modifier
+        .height(50.dp)
+    )
+
+    Column(modifier.padding(16.dp)) {
+      Text(
+        LoremIpsum(50).values.first(),
+        fontSize = 18.sp,
+        fontWeight = FontWeight(700),
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 2
+      )
+
+      Text("R$ 14,99",
+        modifier.padding(8.dp),
+        fontSize = 14.sp,
+        fontWeight = FontWeight(400))
+    }
+  }
 }
 
 
